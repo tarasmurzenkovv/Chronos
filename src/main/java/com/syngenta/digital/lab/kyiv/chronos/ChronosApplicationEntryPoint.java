a137f970f84732c5028d2a88de59669a5c64eb6b
Taras <murzenkovtn@gmail.com>
2018-12-21 13:27:02 +0200

    [2172]: initial commit with the base project structure

---
+++ src/main/java/com/syngenta/digital/lab/kyiv/chronos/ChronosApplicationEntryPoint.java
@@ -0,0 +1,11 @@
+package com.syngenta.digital.lab.kyiv.chronos;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("all")
+@SpringBootApplication
+public class ChronosApplicationEntryPoint {
+    public static void main(String[] args) {
+
+    }
+}
