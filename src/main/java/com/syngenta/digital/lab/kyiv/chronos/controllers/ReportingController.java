044c27bbbfb844f10471babcfdce4b0760384c82
Taras <murzenkovtn@gmail.com>
2019-01-28 14:06:09 +0200

    fixed checkstyle

--- src/main/java/com/syngenta/digital/lab/kyiv/chronos/controllers/ReportingController.java
+++ src/main/java/com/syngenta/digital/lab/kyiv/chronos/controllers/ReportingController.java
@@ -15,15 +15,12 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.beans.PropertyEditorSupport;
 import java.time.LocalDate;
 import java.util.List;
 
