7d74090bee5ff60cce443785c0d8a8bd62232f01
Taras <murzenkovtn@gmail.com>
2019-02-01 13:24:15 +0200

    modified heroku deploy

--- exec.bat
+++ exec.bat
@@ -1,1 +1,1 @@
-gradlew build && heroku deploy:jar ./build/libs/chronos.jar --app chronos-time-tracking
+gradlew build -x test && heroku deploy:jar ./build/libs/chronos.jar --app chronos-time-tracking
